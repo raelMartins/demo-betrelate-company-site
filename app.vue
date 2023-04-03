b39c2dd69e0f0799a4a41c5f5bb6eca802a22237
Martins <olamideakeredolu@gmail.com>
2023-04-03 11:13:53 +0100

    feat: completed the layout for the login and signup pages

--- app.vue
+++ app.vue
@@ -1,5 +1,78 @@
+<script></script>
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <NuxtPage />
 </template>
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Work+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+html,
+body {
+  padding: 0;
+  margin: 0;
+  font-family: 'Work Sans', sans-serif;
+  font-size: 62.5%;
+  scroll-behavior: smooth;
+  ::selection {
+    background-color: #9a79b6;
+    color: #ede7f2;
+  }
+}
+body {
+  max-width: 190rem;
+  margin: auto;
+}
+:root {
+  --text-color: #452461;
+  --primary-color: #673692;
+  --light-purple: #e6d4f6;
+  --very-light-purple: #ede7f2;
+  --form-text: #9a79b6;
+  --form-field: #fbf8fd;
+  --primary-box-shadow: 0.1rem 0.05rem 0.4rem 0.05rem rgba(0, 0, 0, 0.2);
+  --letter-spacing: -0.018216rem;
+  --success-color: #4cd964;
+  --error-color: #ea4335;
+}
+
+input,
+button,
+textarea {
+  font-family: 'Work Sans', sans-serif;
+}
+
+a {
+  color: inherit;
+  text-decoration: none;
+}
+
+// // MEDIA QUERIES PER SCREEN SIZE
+
+// @mixin phones {
+//   @media screen and (max-width: 600px) {
+//     @content;
+//   }
+// }
+
+// @mixin largephone {
+//   @media screen and (min-width: 600px) {
+//     @content;
+//   }
+// }
+
+// @mixin tablet {
+//   @media screen and (min-width: 768px) {
+//     @content;
+//   }
+// }
+
+// @mixin laptop {
+//   @media screen and (min-width: 1040px) {
+//     @content;
+//   }
+// }
+
+// @mixin desktop {
+//   @media screen and (min-width: 1370px) {
+//     @content;
+//   }
+// }
+</style>
---
+++ layouts/login-layout.vue
@@ -0,0 +1,103 @@
+<template>
+  <main class="main">
+    <div class="left">
+      <div class="logo">
+        <SvgsBetrelateSVG />
+      </div>
+      <slot />
+    </div>
+    <div class="right">
+      <SvgsLoginImageSVG />
+    </div>
+  </main>
+</template>
+
+<style lang="scss">
+.main {
+  display: flex;
+  min-height: 100vh;
+  .left {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    padding: 3.2rem 5.5rem;
+    .logo {
+      margin-bottom: 5.2rem;
+    }
+    .page_title {
+      font-style: normal;
+      font-weight: 600;
+      font-size: 3rem;
+      line-height: 3.5rem;
+      letter-spacing: var(--letter-spacing);
+      color: var(--primary-color);
+      margin: 0rem;
+      margin-bottom: 1.6rem;
+    }
+    p {
+      font-weight: 500;
+      font-size: 1.8rem;
+      line-height: 120%;
+      color: #404040;
+      margin: 0rem;
+      max-width: 45rem;
+    }
+    .account_access_form {
+      display: flex;
+      flex-direction: column;
+      margin: 1rem 0rem;
+      .form_field {
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        gap: 0.4rem;
+        margin: 1.8rem 0rem;
+        label {
+          font-weight: 500;
+          font-size: 1.8rem;
+          line-height: 100%;
+          color: #999999;
+        }
+        input {
+          height: 5.6rem;
+          background: #f9f7fb;
+          border: 0.1rem solid #999999;
+          border-radius: 1rem;
+          padding: 0.1rem 1rem;
+          outline: none;
+        }
+      }
+      .submit_button {
+        margin-top: 7rem;
+        margin-bottom: 4rem;
+        button {
+          cursor: pointer;
+          background-color: var(--primary-color);
+          color: white;
+          padding: 1.9rem;
+          min-width: 23rem;
+          text-align: center;
+          font-weight: 500;
+          font-size: 1.8rem;
+          line-height: 100%;
+          border: none;
+          outline: none;
+          border-radius: 0.4rem;
+        }
+      }
+      a {
+        color: var(--primary-color);
+      }
+    }
+  }
+  .right {
+    background-color: #672693;
+    width: 59.7rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: sticky;
+    top: 0;
+  }
+}
+</style>
